4b839affbb117329c635ac6384d78e3335121c61
slackmagic <laurent.pietrzyk@gmail.com>
2021-06-05 21:50:26 +0200

    🎨 Db interface update

--- pkg/db.go
+++ pkg/db.go
@@ -7,19 +7,56 @@ import (
 	"github.com/xujiajun/nutsdb"
 )
 
+var database *nutsdb.DB
 
 func OpenDB(){
 
 	opt := nutsdb.DefaultOptions
 	fmt.Println(opt)
 
-	opt.RWMode = nutsdb.MMap;
-	fmt.Println(opt)
+	//opt.RWMode = nutsdb.MMap;
+	//fmt.Println(opt)
 	
 	opt.Dir = "./data/nutsdb"
 	db, err := nutsdb.Open(opt)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
+
+	database = db
+
+	if err := db.View(
+		func(tx *nutsdb.Tx) error {
+			bucket := "bucket"
+			entries, err := tx.GetAll(bucket)
+			if err != nil {
+				return err
+			}
+	
+			for _, entry := range entries {
+				fmt.Println(string(entry.Key))
+			}
+	
+			return nil
+		}); err != nil {
+		log.Println(err)
+	}
+}
+
+
+
+func CloseDB(){
+	defer database.Close()
+}
+
+func InsertData(key []byte, value []byte){
+	if err := database.Update(
+		func(tx *nutsdb.Tx) error {
+			if err := tx.Put("bucket", key, value, 50); err != nil {
+				return err
+			}
+			return nil
+		}); err != nil {
+		log.Fatal(err)
+	}
 }
